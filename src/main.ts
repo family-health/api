836554d90d0033760d793700b2e26d8c2c4b4814
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-04-05 18:13:36 -0500

    agregado el modulo de open doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  const options = new DocumentBuilder()
+    .setTitle('Family Health API')
+    .setDescription('Scheduled Family Health App')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
