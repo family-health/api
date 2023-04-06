9a198862468d8cd08f5406c8b9596dcf1f740bb2
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-04-05 21:33:17 -0500

    added global intercector

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { TimeOutInterceptor } from './common/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
