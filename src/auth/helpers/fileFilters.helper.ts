87b1b8d9bfb18c8595467a0c260eaae0cc853449
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-04-27 14:53:06 -0500

    actualizacion

--- src/auth/helpers/fileFilters.helper.ts
+++ src/auth/helpers/fileFilters.helper.ts
@@ -2,18 +2,21 @@ export const fileFilters = (req: Express.Request, file: Express.Multer.File, cal
     if (!file) {
         return callback(new Error('File is empty'), false);
     }
-    // // Validar tipo de archivo
-    // const fileType = file.mimetype.split('/')[1];
-    // const validExtensions = ['png', 'jpg', 'jpeg'];
-    // if (!validExtensions.includes(fileType)) {
-    //     return callback(new Error('Invalid file type. Only PNG, JPG and JPEG are allowed.'), false);
-    // }
+    // Validar tipo de archivo
+    const fileType = file.originalname.split('.')[1];
+    
+    const validExtensions = ['png', 'jpg', 'jpeg'];
+    if (!validExtensions.includes(fileType)) {
+        return callback(new Error('Invalid file type. Only PNG, JPG and JPEG are allowed.'), false);
+    }
+    console.log(file);
+    
 
-    // // Validar tamaño de archivo
-    // const maxSize = 5 * 1024 * 1024; // 5 MB
-    // if (file.size > maxSize) {
-    //     return callback(new Error('File is too large. Maximum size is 5 MB.'), false);
-    // }
+    // Validar tamaño de archivo
+    const maxSize = 5 * 1024 * 1024; // 5 MB
+    if (file.size > maxSize) {
+        return callback(new Error('File is too large. Maximum size is 5 MB.'), false);
+    }
 
     callback(null, true);
 };
